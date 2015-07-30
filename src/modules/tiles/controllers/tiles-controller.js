f139136568a18c5ce6c9ef2a08cb2a2c1d31ff30
erundle <erundle@redhat.com>
2015-07-30 12:52:33 -0400

    Adding logos

--- src/modules/tiles/controllers/tiles-controller.js
+++ src/modules/tiles/controllers/tiles-controller.js
@@ -9,4 +9,47 @@ angular.module( 'selfServiceApp.tilesModule' )
         // stash a ref to the controller object, and the various parent objects
         var vm = this;
 
+        vm.tiles = [
+        	{ 
+        		'title': 'Apache',
+        		'subtitle': '$4.00 / Month',
+        		'img': 'apache.jpg'
+        	},
+        	{ 
+        		'title': 'JBoss Data Grid',
+        		'subtitle': '$2.00 / Month',
+        		'img': 'jboss.jpg'
+        	},
+        	{ 
+        		'title': 'JBoss EAP',
+        		'subtitle': '$2.00 / Month',
+        		'img': 'jboss.jpg'
+        	},
+        	{ 
+        		'title': 'JBoss Portal',
+        		'subtitle': '$2.00 / Month',
+        		'img': 'jboss.jpg'
+        	},
+        	{ 
+        		'title': 'JBoss Web Server',
+        		'subtitle': '$4.00 / Month',
+        		'img': 'jboss.jpg'
+        	},
+        	{ 
+        		'title': 'MongoDB',
+        		'subtitle': '$6.50 / Month',
+        		'img': 'mongo.jpg'
+        	},
+        	{ 
+        		'title': 'MySql',
+        		'subtitle': '$4.00 / Month',
+        		'img': 'mysql.jpg'
+        	},
+        	{ 
+        		'title': 'Wordpress',
+        		'subtitle': '$8.00 / Month',
+        		'img': 'wordpress.jpg'
+        	}
+        ];
+
     }] );
